33f4634e9a55701790aa1ccd3ae165c859318c37
cjh <1797793818@qq.com>
2021-02-26 17:09:59 +0800

    - jump comments

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const fs = require('fs')
+const readline = require('readline')
 
 module.exports = function (bundler) {
   bundler.on('bundled', async bundle => {
     const { name } = bundle
+    const fileSteram = fs.createReadStream(name)
+    const r1 = readline.createInterface({
+      input: fileSteram,
+      crlfDelay: Infinity
+    })
+    const result = []
+    let isHeader = false
+    for await (const line of r1) {
+      if (isHeader === false) {
+        if (line.length > 0 && line[0] !== '/') {
+          isHeader = true
+          result.push('var require', 'var parcelRequire')
+        }
+      }
+      result.push(line)
+    }
     fs.writeFileSync(
       name,
-      'var require\nvar parcelRequire\n' + fs.readFileSync(name)
+      result.join('\n')
     )
   })
 }
